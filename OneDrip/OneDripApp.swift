8f0a978b49e0c220f7734968bc62e7aaf6772213
wajdAlmansor <wajd2828@gmail.com>
2025-05-24 22:44:56 +0300

    Initial Commit

---
+++ OneDrip/OneDripApp.swift
@@ -0,0 +1,17 @@
+//
+//  OneDripApp.swift
+//  OneDrip
+//
+//  Created by Wajd on 24/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct OneDripApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
